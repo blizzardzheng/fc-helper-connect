6475082a820db98a2aaa00dcb0cdb97f3665199a
candy.zheng <caifeng.zxq@antfin.com>
2019-04-17 19:12:12 +0800

    init

--- lib/connect.ts
+++ lib/connect.ts
@@ -1,10 +1,28 @@
+const runscript = require('runscript');
+export async function runScriptBatch(commands, options?: object) {
+  const result = {};
+  for (let command of commands) {
+    command = command.name ? command : { name: command, exec: command };
+    try {
+      const stdout = await runscript(command.exec, { ...options, ...(command.option || {})}); 
+      result[command.name] = stdout;
+    } catch(e) {
+      result[command.name] = e;
+    }
+  }
+  return result;
+}
+
 export default function connect(config) {
   if (config.$env !== false) { 
     require('dotenv').config(config.$env || {});
   }
+  if (config.$init) {
+    config.$init(process.env);
+  }
   return function(origin) {
     Object.keys(config)
-    .filter(key => key !== '$env')
+    .filter(key => !key.startsWith('$'))
     .forEach(key => {
       origin[key] = async function(event, _, callback) {
         const eventStr = event.toString();
@@ -12,7 +30,7 @@ export default function connect(config) {
         try {
           queries = JSON.parse(eventStr);
         } catch(e) {}
-        const res = await config[key](queries, callback);
+        const res = await config[key](queries, config, callback);
         if (res) {
           callback(null, res);
         }
